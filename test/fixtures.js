8d0797fa87ab7761df83dccbbcfeeb1173d068f9
Andre <andre.luismp@gmail.com>
2026-03-14 13:21:32 +0100

    test: add pub model tests

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,34 +1,58 @@
 export const serviceUrl = "http://localhost:3000";
 
-export const maggie = {
+export const testUser = {
   firstName: "Maggie",
   lastName: "Simpson",
   email: "maggie@simpson.com",
   password: "secret",
 };
 
-export const maggieCredentials = {
+export const userCredentials = {
   email: "maggie@simpson.com",
   password: "secret",
 };
-
 export const testUsers = [
-  {
-    firstName: "Homer",
-    lastName: "Simpson",
-    email: "homer@simpson.com",
-    password: "secret",
-  },
-  {
-    firstName: "Marge",
-    lastName: "Simpson",
-    email: "marge@simpson.com",
-    password: "secret",
-  },
   {
     firstName: "Bart",
     lastName: "Simpson",
     email: "bart@simpson.com",
     password: "secret",
+  },
+];
+
+export const testNewPub = {
+  name: "The Cobblestone",
+  description: "Traditional pub with live music sessions.",
+  latitude: 53.3498,
+  longitude: -6.2789,
+  categories: {
+    daytime: true,
+    nighttime: true,
+    liveMusic: true,
+    dj: false,
+  },
+};
+
+export const testPubList = [
+  {
+    name: "The Brazen Head",
+    description: "Historic pub in Dublin city.",
+    latitude: 53.3438,
+    longitude: -6.2867,
+    categories: { daytime: true, nighttime: true, liveMusic: false, dj: false },
+  },
+  {
+    name: "Whelan's",
+    description: "Live venue with a lively atmosphere.",
+    latitude: 53.3338,
+    longitude: -6.2651,
+    categories: { daytime: false, nighttime: true, liveMusic: true, dj: true },
+  },
+  {
+    name: "The Stag's Head",
+    description: "Classic Victorian-style pub.",
+    latitude: 53.3442,
+    longitude: -6.2645,
+    categories: { daytime: true, nighttime: true, liveMusic: false, dj: false },
   },
 ];
