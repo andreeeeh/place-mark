c109f53ec73bc8eaef8f5a6b22fa8c090f421fd0
Andre <andre.luismp@gmail.com>
2026-05-01 11:48:56 -0300

    fix: add img field to PubSpecPlus schema

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -49,6 +49,7 @@ export const PubSpecPlus = Joi.object()
       liveMusic: Joi.boolean().default(false),
       dj: Joi.boolean().default(false),
     }).required(),
+    img: Joi.string().allow("").default(""),
     isAuthor: Joi.boolean().default(false),
     __v: Joi.number(),
   })
